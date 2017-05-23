990382271d70972b4a349a626bfa74267eba781b
James Ots <code@jamesots.com>
2017-05-23 21:54:48 +0100

    Add some docs

--- src/z80test.ts
+++ src/z80test.ts
@@ -3,14 +3,38 @@ import { Compiler } from './compiler';
 import * as fs from 'fs';
 
 export class Zat {
-    z80: Z80;
-    memory: number[] = [];
+    public z80: Z80;
+    public memory: number[] = [];
+
+    /**
+     * If ioRead has been set, it will be called when an IO read occurrs.
+     */
     public ioRead: (port: number) => number;
+
+    /**
+     * If ioWrite has been set, it will be called when an IO write occurrs.
+     */
     public ioWrite: (port: number, value: number) => void;
+
+    /**
+     * If memRead has been set, it will be called when a memory read occurrs.
+     * If a number is returned, that value will be used. If undefined is returned,
+     * the value from the internal memory will be used.
+     */
     public memRead: (addr: number) => number;
+
+    /**
+     * If memWrite has been set, it will be called when a memory write occurrs.
+     * If true is returned then no further action is taken. If false is returned,
+     * the value will be written to the internal memory.
+     */
     public memWrite: (addr: number, value: number) => boolean;
 
-    private symbols: {[addr: string]: number};
+    /**
+     * The symbol table, which is created by the ASM80 compiler. All symbols
+     * are in upper case.
+     */
+    public symbols: {[addr: string]: number};
 
     constructor() {
         this.z80 = new Z80({
@@ -53,29 +77,50 @@ export class Zat {
         }
     }
 
+    /**
+     * Compile some Z80 code, using the ASM80 compiler.
+     */
     public compile(code: string) {
         let compiled = new Compiler().compile(code);
         this.symbols = compiled.symbols;
         this.load(Array.from(compiled.data));
     }
 
-    public compileFile(filename) {
+    /**
+     * Compile some Z80 code from a file, using the ASM80 compiler.
+     */
+    public compileFile(filename: string) {
         let buffer = fs.readFileSync(filename);
         return this.compile(buffer.toString());
     }
 
-    public load(mem: number[]) {
-        this.memory.splice(0, mem.length, ...mem);
+    /**
+     * Load some values into memory
+     */
+    public load(mem: number[], start = 0) {
+        this.memory.splice(start, mem.length, ...mem);
     }
 
+    /**
+     * Get the current values of the Z80 registers
+     */
     get registers(): Registers {
         return this.z80.getRegisters();
+    }
+
+    /**
+     * Set the current values of the Z80 registers
+     */
+    set registers(registers: Registers) {
+        this.z80.setRegisters(registers);
     }
 
     /**
      * Run until a HALT is encountered, or number of instructions executed is
      * more than runOptions.steps, or instruction at runOptions.breakAt is
      * reached.
+     * 
+     * Returns the number of instructions executed.
      */
     public run(start?: number | string, runOptions?: RunOptions) {
         if (start !== undefined) {
