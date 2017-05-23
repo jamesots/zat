576c12e51bb371657616d27ffcf369de1049a00d
James Ots <code@jamesots.com>
2017-05-23 17:21:54 +0100

    Show registers

--- src/index.ts
+++ src/index.ts
@@ -29,8 +29,17 @@ showRegisters(registers);
 
 function showRegisters(registers: Registers) {
     console.log(
-`A: ${hex8(registers.a)}  A': ${hex8(registers.a_prime)}
-PC: ${hex16(registers.pc)}`);
+`AF: ${hex8(registers.a)}${hex8(flagsToNumber(registers.flags))}  AF': ${hex8(registers.a_prime)}${hex8(flagsToNumber(registers.flags_prime))}
+BC: ${hex8(registers.b)}${hex8(registers.c)}  BC': ${hex8(registers.b_prime)}${hex8(registers.c_prime)}
+DE: ${hex8(registers.d)}${hex8(registers.e)}  DE': ${hex8(registers.d_prime)}${hex8(registers.e_prime)}
+HL: ${hex8(registers.h)}${hex8(registers.l)}  HL': ${hex8(registers.h_prime)}${hex8(registers.l_prime)}
+IX: ${hex16(registers.ix)}   IY: ${hex16(registers.iy)}
+PC: ${hex16(registers.pc)}   SP: ${hex16(registers.sp)}
+ I: ${hex16(registers.i)}    R: ${hex16(registers.r)}
+    S Z Y H X P N C
+ F: ${registers.flags.S} ${registers.flags.Z} ${registers.flags.Y} ${registers.flags.H} ${registers.flags.X} ${registers.flags.P} ${registers.flags.N} ${registers.flags.C}
+F': ${registers.flags_prime.S} ${registers.flags_prime.Z} ${registers.flags_prime.Y} ${registers.flags_prime.H} ${registers.flags_prime.X} ${registers.flags_prime.P} ${registers.flags_prime.N} ${registers.flags_prime.C}
+`);
 }
 
 function hex8(num: number): string {
@@ -41,4 +50,15 @@ function hex8(num: number): string {
 function hex16(num: number): string {
     let hex = num.toString(16);
     return '0000'.substring(0, 4 - hex.length) + hex;
+}
+
+function flagsToNumber(flags: Flags): number {
+    return flags.S << 7
+        | flags.Z << 6
+        | flags.Y << 5
+        | flags.H << 4
+        | flags.X << 3
+        | flags.P << 2
+        | flags.N << 1
+        | flags.C << 0
 }
